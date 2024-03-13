d64ea1bdccd255518b6b127f5fcf0ac5f4ce1f9b
Dinh Van Lanh <dinhlanh@suuforest.net>
2024-03-13 14:08:47 +0700

    update code

--- index.php
+++ index.php
@@ -1,36 +1,14 @@
 <?php
-$servername = "127.0.0.1";
+$servername = "localhost";
 $username = "taoanhpro";
 $password = "taoanhpro";
 $dbname = "taoanhpro";
+$port = "3306"; // Thay 3307 bằng số cổng MySQL bạn muốn sử dụng
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Thiết lập chế độ lỗi để bật chế độ báo lỗi khi có lỗi xảy ra
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Kết nối thành công";
 } catch(PDOException $e) {
     echo "Kết nối thất bại: " . $e->getMessage();
-}
-
-echo "<br>";
-try {
-//   // Ví dụ: Thực hiện truy vấn SELECT
-//   $sql = "SELECT * FROM keyword_temps";
-//   $result = $conn->query($sql);
-//   var_dump($result->rowCount());
-
-//   // Xử lý kết quả
-//   if ($result->rowCount() > 0) {
-//       while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-//           // Xử lý dữ liệu
-//           echo "Column 1: " . $row['keyword'] . "<br>";
-//           echo "Column 2: " . $row['slug'] . "<br>";
-//           // và cứ tiếp tục với các cột khác
-//       }
-//   } else {
-//       echo "Không có dữ liệu trả về từ truy vấn.";
-//   }
-} catch(PDOException $e) {
-  echo "Lỗi truy vấn: " . $e->getMessage();
 }
